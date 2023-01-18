caf7ffd5ba70a8eb4d1e4684d057eeec2797e519
MyUsername <my@email.com>
2023-01-18 16:43:47 +0100

    Still create header layout

--- proj-html-vuejs/src/App.vue
+++ proj-html-vuejs/src/App.vue
@@ -3,12 +3,32 @@ import AppHeader from './components/AppHeader.vue'
 import AppMain from './components/AppMain.vue'
 import AppFooter from './components/AppFooter.vue'
 
+export default {
+  name : 'AppVue',
+  components: {
+    AppHeader,
+    AppMain,
+    AppFooter
+  }
+}
+
 </script>
 
 <template>
 
+<section>
+  <AppHeader/>
+</section>
+
+
+
+
 </template>
 
-<style scoped>
+<style lang="scss" >
+@use './styles/general.scss' as *;
+@use './styles/variables' as *;
+@use 'bootstrap/scss/bootstrap.scss' as *;
+
 
 </style>
--- proj-html-vuejs/src/components/AppHeader.vue
+++ proj-html-vuejs/src/components/AppHeader.vue
@@ -5,13 +5,61 @@ export default {
 }
 </script>
 
-<template lang="">
-   <div>
-      
+<template lang="">   
+   <div class="container-img">
+      <div class="navBar">
+         <div class="img-advBox">
+
+         </div>
+         <div class="nav-list">
+
+         </div>
+      </div>
+
+      <div class="textBox">
+
+      </div>
+   </div>
+
+   <div class="gameSquad">
+
    </div>
 </template>
 
 
-<style lang="scss">
+<style lang="scss" scoped>
+   .container-img{
+      height: 90vh;
+      width: 100%;
+      background-color: rgb(110, 233, 9);
    
+      .navBar{
+         height: 30vh;
+         width: 100%;
+         background-color: rgba(0, 0, 0, 0.468);
+      
+
+         .img-advBox{
+            height: 50%;
+            background-color: blue;
+         }
+
+         .nav-list{
+            height: 50%;
+            background-color: blueviolet;
+         }
+      }
+
+
+      .textBox{
+         height: 60vh;
+         background-color: chocolate;
+      }
+   }
+
+   .gameSquad{
+      height: 30vh;
+      background-color: cornflowerblue;
+   }
+
 </style>
