8124337816aa8daf563f7574944776dfcd01f753
MyUsername <my@email.com>
2023-01-18 18:25:03 +0100

    still create header

--- proj-html-vuejs/src/components/AppHeader.vue
+++ proj-html-vuejs/src/components/AppHeader.vue
@@ -16,14 +16,57 @@ export default {
             </div>
          </div>
          <div class="nav-list">
-
+            <ul>
+               <li>
+                  Home
+               </li>
+               <li>
+                  Fixtures & Result
+               </li>
+               <li>
+                  League Table
+               </li>
+               <li>
+                  Players
+               </li>
+               <li>
+                  Gallery
+               </li>
+               <li>
+                  Blog
+               </li>
+               <li>
+                  Contact
+               </li>
+            </ul>
          </div>
       </div>
 
       <div class="textBox">
+         <div class="text">
+               <h1>
+                  Football Club 
+               </h1>
+               <h1>
+                  Sport Club
+               </h1>
+               
+               <br>
 
+               <h4>
+                  Private football matches
+               </h4>
+
+               <div class="learnMore">
+
+                  <h4 class="btn">
+                     Learn More
+                  </h4>
+               </div>
+            </div>
+         </div>
+         
       </div>
-   </div>
 
    <div class="gameSquad">
 
@@ -33,7 +76,7 @@ export default {
 
 <style lang="scss" scoped>
    .container-img{
-      height: 90vh;
+      height: 110vh;
       width: 100%;
       background-image: url(../assets/img/slider2-1.jpg);
       background-repeat: no-repeat;
@@ -47,9 +90,11 @@ export default {
 
          .img-advBox{
             display: flex;
-            height: 75%;
+            height: 70%;
             position: relative;
             margin-left:  55rem;
+
+
 
 
             .advHeader{
@@ -65,12 +110,50 @@ export default {
 
          .nav-list{
             height: 30%;
+            
+            ul{
+               display: flex;
+               color: white;
+               list-style-type: none;
+               text-transform: uppercase;
+               
+
+               li{
+                  margin-left: 8rem;
+                  margin-top: 1.5rem;
+                  font-size: 1.5rem;
+               }
+            }
          }
       }
 
 
       .textBox{
-         height: 60vh;
+         height: 70vh;
+         text-align: center;
+         margin-top: 10rem;
+
+         h1{
+            color: white;
+            font-size: 5rem;
+         }
+
+         h4{
+            color: white;
+            font-weight: 700;
+         }
+
+         .learnMore{ 
+            margin: 5rem;
+
+            h4{
+               height: 5rem;
+               width: 25rem;
+               padding: 2rem;
+               border: 2px solid white;
+               border-radius: 45%;
+            }
+         }
       }
    }
 
