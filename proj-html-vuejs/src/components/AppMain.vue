da20b2425444c45379b6615484e10a3a44b37257
MyUsername <my@email.com>
2023-01-19 17:26:59 +0100

    Starting to create a result table, complete res tb

--- proj-html-vuejs/src/components/AppMain.vue
+++ proj-html-vuejs/src/components/AppMain.vue
@@ -147,11 +147,334 @@ export default {
    <div class="containerTable">
       <div class="leagueTable">
          <div class="titleOne text-center">
-         <h1>League Table</h1>
+            <h1>League Table</h1>
+         </div>
+
+         <div class="topTable d-flex">
+            <div class="LeftTable">
+               <h1>
+                  Team
+               </h1>
+            </div>
+            <div class="RightTable d-flex">
+               <h1>
+                  W
+               </h1>
+               <h1>
+                  D
+               </h1>
+               <h1>
+                  L
+               </h1>
+               <h1>
+                  PTS
+               </h1>
+            </div>
+         </div>
+
+
+         <div class="bannerMatch d-flex">
+            <div class="leftBanner d-flex">
+               <h4>
+                  1
+               </h4>
+               <img src="../assets/img/league-club-1.png" alt="">
+               <h4>
+                  Istanbul Sports
+               </h4>
+            </div>
+            <div class="rightBanner d-flex">
+               <h4>
+                  18
+               </h4>
+
+               <h4>
+                  2
+               </h4>
+
+               <h4>
+                  0
+               </h4>
+
+               <h4 class="fw-bolder">
+                  42
+               </h4>
+            </div>
+         </div>
+
+         <div class="bannerMatch d-flex">
+            <div class="leftBanner d-flex">
+               <h4>
+                  2
+               </h4>
+               <img src="../assets/img/league-club-2.png" alt="">
+               <h4>
+                  United Fs Club
+               </h4>
+            </div>
+            <div class="rightBanner d-flex">
+               <h4>
+                  14
+               </h4>
+
+               <h4>
+                  2
+               </h4>
+
+               <h4>
+                  1
+               </h4>
+
+               <h4 class="fw-bolder">
+                  35
+               </h4>
+            </div>
+         </div>   
+
+         <div class="bannerMatch d-flex">
+            <div class="leftBanner d-flex">
+               <h4>
+                  3
+               </h4>
+               <img src="../assets/img/league-club-3.png" alt="">
+               <h4>
+                  Schalke Club
+               </h4>
+            </div>
+            <div class="rightBanner d-flex">
+               <h4>
+                  13
+               </h4>
+
+               <h4>
+                  3
+               </h4>
+
+               <h4>
+                  0
+               </h4>
+
+               <h4 class="fw-bolder">
+                  32
+               </h4>
+            </div>
+         </div>  
+
+         <div class="bannerMatch d-flex">
+            <div class="leftBanner d-flex">
+               <h4>
+                  4
+               </h4>
+               <img src="../assets/img/league-club-4.png" alt="">
+               <h4>
+                  Inter Club
+               </h4>
+            </div>
+            <div class="rightBanner d-flex">
+               <h4>
+                  13
+               </h4>
+
+               <h4>
+                  2
+               </h4>
+
+               <h4>
+                  1
+               </h4>
+
+               <h4 class="fw-bolder">
+                  31
+               </h4>
+            </div>
+         </div>  
+
+         <div class="bannerMatch d-flex">
+            <div class="leftBanner d-flex">
+               <h4>
+                  5
+               </h4>
+               <img src="../assets/img/league-club-5.png" alt="">
+               <h4>
+                  Madrid Club
+               </h4>
+            </div>
+            <div class="rightBanner d-flex">
+               <h4>
+                  12
+               </h4>
+
+               <h4>
+                  1
+               </h4>
+
+               <h4>
+                  2
+               </h4>
+
+               <h4 class="fw-bolder">
+                  30
+               </h4>
+            </div>
+         </div>  
+
+         <div class="bannerMatch d-flex">
+            <div class="leftBanner d-flex">
+               <h4>
+                  6
+               </h4>
+               <img src="../assets/img/league-club-6.png" alt="">
+               <h4>
+                  Schalke Club
+               </h4>
+            </div>
+            <div class="rightBanner d-flex">
+               <h4>
+                  11
+               </h4>
+
+               <h4>
+                  0
+               </h4>
+
+               <h4>
+                  3
+               </h4>
+
+               <h4 class="fw-bolder">
+                  28
+               </h4>
+            </div>
+         </div>  
+
+         <div class="bannerMatch d-flex">
+            <div class="leftBanner d-flex">
+               <h4>
+                  7
+               </h4>
+               <img src="../assets/img/league-club-7.png" alt="">
+               <h4>
+                  Arsenal Club
+               </h4>
+            </div>
+            <div class="rightBanner d-flex">
+               <h4>
+                  10
+               </h4>
+
+               <h4>
+                  5
+               </h4>
+
+               <h4>
+                  0
+               </h4>
+
+               <h4 class="fw-bolder">
+                  26
+               </h4>
+            </div>
+         </div>  
+
+         <div class="bannerMatch d-flex">
+            <div class="leftBanner d-flex">
+               <h4>
+                  8
+               </h4>
+               <img src="../assets/img/league-club-8.png" alt="">
+               <h4>
+                  Bayern Club
+               </h4>
+            </div>
+            <div class="rightBanner d-flex">
+               <h4>
+                  10
+               </h4>
+
+               <h4>
+                  2
+               </h4>
+
+               <h4>
+                  2
+               </h4>
+
+               <h4 class="fw-bolder">
+                  24
+               </h4>
+            </div>
+         </div>  
+
+         <div class="bannerMatch d-flex">
+            <div class="leftBanner d-flex">
+               <h4>
+                  9
+               </h4>
+               <img src="../assets/img/league-club-9.png" alt="">
+               <h4>
+                  Atletico Club
+               </h4>
+            </div>
+            <div class="rightBanner d-flex">
+               <h4>
+                  19
+               </h4>
+
+               <h4>
+                  0
+               </h4>
+
+               <h4>
+                  4
+               </h4>
+
+               <h4 class="fw-bolder">
+                  20
+               </h4>
+            </div>
+         </div>  
+
+         <div class="ViewButton">
+            <h4>
+               View Full League Table
+            </h4>
+         </div>
       </div>
-      </div>
+
+
+
       <div class="resultTable">
-         
+         <div class="titleTwo text-center">
+            <h1>League Table</h1>
+         </div>
+
+         <div class="resultBannerOne">
+
+         </div>
+         <div class="resultBannerTwo">
+
+         </div>
+         <div class="resultBannerOne">
+
+         </div>
+         <div class="resultBannerTwo">
+
+         </div>
+         <div class="resultBannerOne">
+
+         </div>
+         <div class="resultBannerTwo">
+
+         </div>
+         <div class="resultBannerOne">
+
+         </div>
+         <div class="resultBannerTwo">
+
+         </div>
+         <div class="resultBannerOne">
+
+         </div>
       </div>
 
    </div>
@@ -297,22 +620,126 @@ export default {
 
    .containerTable{
       height: 100vh;
-      background-color: rgb(201, 57, 9);
+      background-color: #ffffff;
       display: flex;
+
+
+      .titleOne{
+         padding: 1rem;
+         color: rgb(0, 0, 0);
+
+         h1{
+            font-size: 2rem;
+            font-weight: 700;
+         }
+      }
 
       .leagueTable{
          height: 85vh;
          width: 45%;
-         background-color: rgba(137, 43, 226, 0.467);
          margin: 2rem;
+         border: .5px soldi gray;
+
+         .ViewButton{
+            height: 7vh;
+            background-color: black;
+
+            h4{
+               font-size: 1.2rem;
+               padding: 1rem;
+               text-align: center;
+               font-weight: 700;
+               color: white;
+            }
+         }
+
+
+         .bannerMatch{
+            height: 8vh;
+            width: 100%;
+            background-color: #f5f5f5;
+
+
+            .leftBanner{
+               width: 50%;
+
+               h4{
+                  font-size: 1.3rem;
+                  font-weight: 200;
+                  padding: 1.2rem;
+               }
+            }
+
+            .rightBanner{
+               width: 50%;
+
+               h4{
+                  margin-top: .3rem;
+                  font-size: 1.3rem;
+                  font-weight: 200;
+                  padding: .9rem;
+                  margin-left: 3rem;
+               }
+            }
+         }
       }
 
       
       .resultTable{
          height: 85vh;
-         width: 45%;
-         background-color: rgba(43, 226, 119, 0.395);
+         width: 40%;
          margin: 2rem;
+
+         .resultBannerOne{
+            height: 8.5vh;
+            background-color: #212121;
+         }
+
+         
+         .resultBannerTwo{
+            height: 8.5vh;
+            background-color: rgb(197, 112, 7);
+         }
+
+         .titleTwo{
+            padding: 1rem;
+            
+            h1{
+               font-size: 2rem;
+               font-weight: 700;
+            }
+         }
+      }
+
+
+      .topTable{
+         width: 100%;
+         height: 5vh;
+         background-color: #212121;
+         color: white;
+
+         .LeftTable{
+            width: 50%;
+
+            h1{
+               font-size: 1.5rem;
+               color: white;
+               margin-left: .5rem;
+               margin-top: .5rem;
+            }
+         }
+
+         .RightTable{
+            width: 50%;
+
+            h1{
+               font-size: 1.5rem;
+               color: white;
+               margin-left: 4.3rem;
+               margin-top: .5rem;
+            }
+         }
+
       }
 
 
